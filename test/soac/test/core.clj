03dbada9f3563c1507fae6756cffc4cc1cad8bef
Bryce Nyeggen <bryce.nyeggen@livingsocial.com>
2012-09-29 15:24:36 -0400

    Additional tests

--- test/soac/test/core.clj
+++ test/soac/test/core.clj
@@ -7,27 +7,37 @@
   (let [a (repeatedly 1000 #(vector (rand) (rand-int 100) (rand)))
         soa (make-SOA :double :int :double)]
     (is (.isEmpty soa))
+    
     (.addAll soa a)
     (is (== 1000 (count soa)))
+    (is (not (.isEmpty soa)))
+    
     (is (= soa a))
     (is (not (= 7 soa)))
     (is (not (= [] soa)))
+    
     (trim! soa)
     (is (= soa a))
         
     (is (not (.contains soa [0.0 -1 0.0])))
     (is (== -1 (.indexOf soa [0.0 -1 0.0])))
+    
     (.add soa [0.0 -1 0.0])
+    (is (== (count soa) 1001))
     (is (.contains soa [0.0 -1 0.0]))
     (is (== 1000 (.indexOf soa [0.0 -1 0.0])))
     
     (.remove soa (int 1000))
     (is (not (.contains soa [0.0 -1 0.0])))
+    (is (== (count soa) 1000))
     
     (let [f-elem (first soa)]
       (is (.contains soa f-elem))
       (.remove soa f-elem)
-      (is (not (.contains soa f-elem))))))
+      (is (not (.contains soa f-elem))))
+    (.clear soa)
+    (is (.isEmpty soa))
+    (is (== 0 (count soa)))))
 
 (deftest test-sorting
   (let [soa (doto (make-SOA :double :int :double)
