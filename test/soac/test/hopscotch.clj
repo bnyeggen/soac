bcf95c26367250b878e61e5ef31e5d04c8de8b8a
Bryce Nyeggen <fiat.money@gmail.com>
2013-08-11 12:48:39 -0700

    Test removal

--- test/soac/test/hopscotch.clj
+++ test/soac/test/hopscotch.clj
@@ -16,3 +16,18 @@
     (is (== (count b) (count c)))
     (is (every? #(contains? c %) (keys b)))
     (is (every? #(= (get b %) (get c %)) (keys b)))))
+
+(deftest test-set-disj
+  (let [a (into #{} (repeatedly 100 #(rand-int 200)))
+        b (take 30 (shuffle (vec a)))
+        d (reduce disj (into (prim-hash-set :int) a) b)
+        e (reduce disj a b)]
+    (is (= d e))))
+
+(deftest test-map-disj
+  (let [a (into {} (repeatedly 100 #(vector (rand-int 200) (rand))))
+        b (into (prim-hash-map :int :double) a)
+        c (take 30 (shuffle (keys a)))
+        d (reduce dissoc a c)
+        e (reduce dissoc b c)]
+    (is (= d e))))
