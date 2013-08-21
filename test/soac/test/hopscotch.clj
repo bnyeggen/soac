ffc6e5fd02812b3e7f1a39d747dd142022a156e5
Bryce Nyeggen <fiat.money@gmail.com>
2013-08-20 21:57:03 -0700

    More tests

--- test/soac/test/hopscotch.clj
+++ test/soac/test/hopscotch.clj
@@ -1,6 +1,7 @@
 (ns soac.test.hopscotch
   (:use clojure.test
-        soac.hopscotch))
+        soac.hopscotch)
+  (:import [java.util HashMap HashSet]))
 
 (deftest test-set
   (let [a (prim-hash-set :int)
@@ -22,7 +23,8 @@
         b (take 30 (shuffle (vec a)))
         d (reduce disj (into (prim-hash-set :int) a) b)
         e (reduce disj a b)]
-    (is (= d e))))
+    (is (= d e))
+    (is (empty? (reduce disj (into (prim-hash-set :int) (range 100)) (range 100))))))
 
 (deftest test-map-disj
   (let [a (into {} (repeatedly 100 #(vector (rand-int 200) (rand))))
@@ -41,3 +43,28 @@
 (deftest test-boxing
   (is (contains? (conj (prim-hash-set :int) (int 4)) (long 4)))
   (is (== 1 (count (into (prim-hash-set :int) [(long 4) (int 4)])))))
+
+(deftest ^:performance test-speed
+  (let [to-insert (long-array (repeatedly 1000000 #(rand-int Integer/MAX_VALUE)))
+        clj-set (do (print "Clojure set insert: ")
+                  (time (into #{} to-insert)))
+        java-set (do (print "Java set insert: ")
+                   (time (let [a (HashSet.)]
+                           (doseq [e to-insert] (.add ^HashSet a e))
+                           a)))
+        imm-set (do (print "Immutable primitive set insert: ")
+                  (time (into (prim-hash-set :long) to-insert)))]
+    
+    (print "Clojure set lookup: ")
+    (time (every? #(contains? clj-set %) to-insert))
+    (print "Java set lookup: ")
+    (time (every? #(.contains ^HashSet java-set %) to-insert))
+    (print "Immutable primitive set lookup: ")
+    (time (every? #(contains? imm-set %) to-insert))
+    
+    (print "Clojure set removal: ")
+    (time (reduce disj clj-set to-insert))
+    (print "Java set removal: ")
+    (time (doseq [e to-insert] (.remove ^HashSet java-set e)))
+    (print "Immutable primitive set removal: ")
+    (time (reduce disj imm-set to-insert))))
