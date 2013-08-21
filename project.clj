85abf0c5f2a969e0ca6438426dc8cc4c2f52a17d
Bryce Nyeggen <fiat.money@gmail.com>
2013-08-20 19:01:31 -0700

    No lein 1.x compatiblity

--- project.clj
+++ project.clj
@@ -1,9 +1,5 @@
 (defproject soac "0.1.4-SNAPSHOT"
   :description "Flexible structs-of-arrays"
   :url "https://github.com/fiatmoney/soac"
-  ;I believe this is a Leiningen 2 option, currently no raw Java but may be
-  ;some later
   :java-source-paths ["java-src"]
-  ;And this is the Leiningen 1.7.x version
-  :java-source-path "java-src"
   :dependencies [[org.clojure/clojure "1.5.1"]])
