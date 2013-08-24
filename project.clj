d0372ff672bd077ecf4689bfd5f96cca6f4bf574
Bryce Nyeggen <fiat.money@gmail.com>
2013-08-24 11:44:54 -0700

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,8 +1,10 @@
-(defproject soac "0.1.5"
-  :description "Flexible structs-of-arrays"
+(defproject com.nyeggen/soac "0.1.6-SNAPSHOT"
+  :description "Primitive-backed collections for Clojure"
   :url "https://github.com/fiatmoney/soac"
   :java-source-paths ["java-src"]
   :dependencies [[org.clojure/clojure "1.5.1"]]
+  :min-lein-version "2.0.0"
+  :javac-options ["-target" "1.7" "-source" "1.7"]
   :test-selectors {:default (complement :performance)
                    :performance :performance
                    :all (constantly true)})
